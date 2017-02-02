f6ff772e123108c20cab9acd430a96588520ecb6
Alena Kuntsevich <ekuntsevich@yandex.ru>
2017-02-02 23:06:34 +0300

    tic-tac-toe/feature: complete

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,19 +1,21 @@
 class TicTacToe {
     constructor() {
+
         this.storage =[ [  null, null ,null ], [  null, null ,null ], [  null, null ,null ] ];
         this.player ='x';
-        this.winner = 1;
+        this.winner=null;
         this.count =0;
+
     }
 
     getCurrentPlayerSymbol() {
-
 
         return this.player;
 
     }
 
     nextTurn(rowIndex, columnIndex) {
+
         if (this.storage[rowIndex][columnIndex] == null){
             this.storage[rowIndex][columnIndex] = this.player;
             this.count++;
@@ -21,51 +23,46 @@ class TicTacToe {
                 this.player ='o';
 
 
-            }else {
-                this.player = 'x';
+           }else {
+             this.player = 'x';
 
-            }
+           }
 
         }
 
-
-        if (this.storage[0][0]==this.player && this.storage[0][1]==this.player && this.storage[0][2]==this.player){
-            this.winner =this.player;
-         }
-       else if (this.storage[1][0]==this.player && this.storage[1][1]==this.player && this.storage[1][2]==this.player){
-            this.winner =this.player;
-        }  else if (this.storage[2][0]==this.player && this.storage[2][1]==this.player && this.storage[2][2]==this.player){
-            this.winner =this.player;
-        } else if (this.storage[0][0]==this.player && this.storage[1][1]==this.player && this.storage[2][2]==this.player){
-            this.winner =this.player;
-        } else if (this.storage[0][2]==this.player && this.storage[1][1]==this.player && this.storage[2][0]==this.player){
-        this.winner =this.player;
-    } else if (this.storage[0][0]==this.player && this.storage[1][0]==this.player && this.storage[2][0]==this.player){
-            this.winner =this.player;
-        } else if (this.storage[0][1]==this.player && this.storage[1][1]==this.player && this.storage[2][1]==this.player){
-            this.winner =this.player;
-        }else if (this.storage[0][2]==this.player && this.storage[1][2]==this.player && this.storage[2][2]==this.player){
-            this.winner =this.player;
+        if (this.storage[0][0]!=null && this.storage[0][1]==this.storage[0][0] && this.storage[0][2]==this.storage[0][1]){
+            this.winner =this.storage[0][0];
+         } else if (this.storage[1][0]!=null && this.storage[1][1] == this.storage[1][0] &&this.storage[1][1] == this.storage[1][2]){
+            this.winner =this.storage[1][0];
+        } else if (this.storage[2][0]!=null && this.storage[2][1]==this.storage[2][0] && this.storage[2][2]==this.storage[2][1]){
+            this.winner =this.storage[2][0];
+        } else if (this.storage[0][0]!=null && this.storage[1][1]==this.storage[0][0] && this.storage[2][2]==this.storage[0][0]){
+            this.winner =this.storage[0][0];
+        } else if (this.storage[0][2]!=null && this.storage[1][1]==this.storage[0][2] && this.storage[2][0]==this.storage[0][2]){
+            this.winner =this.storage[0][2];
+        } else if (this.storage[0][0]!=null && this.storage[1][0]==this.storage[0][0] && this.storage[2][0]==this.storage[1][0]){
+            this.winner =this.storage[0][0];
+        } else if (this.storage[0][1]!=null && this.storage[1][1]==this.storage[0][1] && this.storage[2][1]==this.storage[1][1]){
+            this.winner =this.storage[0][1];
+        }else if (this.storage[0][2]!=null && this.storage[1][2]==this.storage[0][2] && this.storage[2][2]==this.storage[0][2]){
+            this.winner =this.storage[0][2];
         }
 
-
-
-
-
+   return this;
     }
 
     isFinished() {
-   /*if (this.count ==9) {
-       return null;
-   } else if(this.winner == 'x') {
-       return 'o';
-   } else if(this.winner == 'o'){
-       return 'x';*/
+
+   if(this.winner == null && this.count!=9) {
+       return false;
+   } else {
+       return true;
     }
+
 }
 
     getWinner() {
-
+        return this.winner;
     }
 
     noMoreTurns() {
@@ -79,7 +76,10 @@ class TicTacToe {
 
     isDraw() {
 
-
+    if (this.count == 9 && this.winner == null){
+        return true;
+    }else {
+        return false;
         }
     }
 
